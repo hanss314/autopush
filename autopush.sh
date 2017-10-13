ec1ee9073b19f9d64d11bc11894c5ccb8d50f9e5
hanss314 <hanss314@gmail.com>
2017-10-12 21:52:53 -0700

    Added files

--- autopush.sh
+++ autopush.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-uname=$1
-pwd=$2
 origin=`git remote show origin | grep -o -P "(?<=Push  URL: )http(s?)://.*\..*\/.*\.git"`
 http=`echo $origin | grep -o -P "http(s?)://"`
 repo=`echo $origin | grep -o -P "(?<=:\/\/).*\..*\/.*\.git"`
-origin=$http$uname:$pwd@$repo
+origin=$http$1:$2@$repo
 if git push --repo "$origin"; then
 	echo "Credentials okay"
 else
 	echo "Credentials not okay"
 	exit 1
 fi
+
 while true
 do
-	echo "hello"
+	[ -n "`git status -s`" ] && git add . && git commit -m "Added files" && git push --repo "$origin"
 done
