f9d2234883bde9ae577c648745a1e9b25f1851ba
hanss314 <hanss314@gmail.com>
2017-10-12 20:30:37 -0700

    Added credentials check

--- autopush.sh
+++ autopush.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+uname=$1
+pwd=$2
+origin=`git remote show origin | grep -o -P "(?<=Push  URL: )http(s?)://.*\..*\/.*\.git"`
+http=`echo $origin | grep -o -P "http(s?)://"`
+repo=`echo $origin | grep -o -P "(?<=:\/\/).*\..*\/.*\.git"`
+origin=$http$uname:$pwd@$repo
+if git push --repo "$origin"; then
+	echo "Credentials okay"
+else
+	echo "Credentials not okay"
+	exit 1
+fi
 while true
 do
 	echo "hello"
